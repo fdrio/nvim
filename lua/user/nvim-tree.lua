c57e0c02b41cacc2f84e2695ceecfbdca83bd6a5
francisco-grata <127760057+francisco-grata@users.noreply.github.com>
2023-04-18 11:56:03 -0400

    Removed deprecated section

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -57,13 +57,6 @@ update_focused_file = {
   view = {
 --    width = 30,
 --    height = 30,
-    side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
+      side = "left",
     },
-  },
 }
