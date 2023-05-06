4a790fba90889fef169a922f1f137b06d799e668
Francisco Diaz <frco3190@gmail.com>
2023-05-05 21:51:33 -0400

    Render types and keywords bold

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,19 @@
-vim.cmd [[
-try
-  colorscheme carbonfox 
-  set background=dark
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=light
-endtry
-]]
+require('nightfox').setup({
+  options = {
+    styles = {
+	  comments = "italic",
+      keywords = "bold",
+      types = "bold",
+    }
+  }
+})
+vim.cmd("colorscheme carbonfox")
+--vim.cmd [[
+--try
+--  colorscheme carbonfox 
+--  set background=dark
+--catch /^Vim\%((\a\+)\)\=:E185/
+--  colorscheme default
+--  set background=light
+--endtry
+--]]
